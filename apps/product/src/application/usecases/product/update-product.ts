330f46d65c69014442834d30456141616fd36354
bene <matheusbenediht@gmail.com>
2023-06-07 17:06:17 -0300

    Adicionando metodo Update

---
+++ apps/product/src/application/usecases/product/update-product.ts
@@ -0,0 +1,32 @@
+import { prismaClient } from "../../../infra/database/prismaClient";
+type UpdateProductRequest={
+    name: string;
+    description: string;
+    price: number;
+    quantity: number;
+    bar_code: string;
+    createdAt: Date;
+}
+
+export class UpdateProductUseCase{
+    constructor(){}
+
+    async execute(id: string, data: UpdateProductRequest){
+        const productExist = await prismaClient.product.findUnique({
+            where: {
+                id,
+            }
+        });
+        if(!productExist){
+            throw new Error("Product does not exist");
+        }
+
+        return await prismaClient.product.update({
+            data,
+            where: {
+                id,
+            }
+        })
+}
+
+}
--- apps/product/src/infra/http/routes.ts
+++ apps/product/src/infra/http/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { RegisterProductController } from "../../application/controllers/product/product-register.controller";
 import { DeleteProductController } from "../../application/controllers/product/product-delete.controller";
 import { IndexProductController } from "../../application/controllers/product/product-index.controller";
+import { UpdateProductController } from "../../application/controllers/product/product-update.controller";
 
 const router = Router();
 
@@ -15,5 +16,9 @@ router.post('/product/register', (req,res) => {
 
 router.delete('/product/delete/:id', (req,res) => {
     new DeleteProductController().handle(req,res)
+})
+
+router.put('/product/edit/:id', (req,res) => {
+    new UpdateProductController().handle(req,res)
 })
 export {router}
