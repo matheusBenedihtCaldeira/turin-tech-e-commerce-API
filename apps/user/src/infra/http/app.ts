c2c3aa9c99acd0ab93c36b93e5a67c04dd847b42
bene <matheusbenediht@gmail.com>
2023-06-08 14:23:17 -0300

    Criando diretorio do serviço User

---
+++ apps/user/src/infra/http/app.ts
@@ -0,0 +1,10 @@
+import express, { Router } from 'express';
+import router from './routes/user';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(router);
+
+export default app
---
+++ apps/user/src/infra/http/server.ts
@@ -0,0 +1,7 @@
+import app from "./app";
+
+const PORT = process.env.SERVER_PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port http://localhost:${PORT}/`)
+})
