35b9ed659e234e31bc0909cc7bb33c33be74bbc8
bene <matheusbenediht@gmail.com>
2023-06-11 20:33:27 -0300

    Adicionando validação

--- apps/customer/src/application/usecases/get-customer.ts
+++ apps/customer/src/application/usecases/get-customer.ts
@@ -2,10 +2,13 @@ import { prismaClient } from "../../infra/database/prismaClient";
 
 export class GetCustomerUseCase {
     async execute(id: string){
-        return await prismaClient.customer.findUnique({
+
+        const customer = await prismaClient.customer.findUnique({
             where: {
                 id
             }
         })
+       if(!customer) throw new Error("Customer not found")
+        return customer
     }
 }
