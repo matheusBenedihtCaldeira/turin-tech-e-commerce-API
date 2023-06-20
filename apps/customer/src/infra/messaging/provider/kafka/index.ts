acb96f0486c7b713f4d50b925c5204962d9d6d50
bene <matheusbenediht@gmail.com>
2023-06-20 20:27:31 -0300

    Config

--- apps/customer/src/infra/messaging/provider/kafka/index.ts
+++ apps/customer/src/infra/messaging/provider/kafka/index.ts
@@ -1,11 +1,15 @@
 import { Kafka } from 'kafkajs';
+import dotenv from 'dotenv';
+dotenv.config();
 
+const KAFKA_USERNAME = process.env.KAFKA_USERNAME as string;
+const KAFKA_PASSWORD = process.env.KAFKA_PASSWORD as string;
 const kafka = new Kafka({
   brokers: ['fun-sheepdog-11334-us1-kafka.upstash.io:9092'],
   sasl: {
     mechanism: 'scram-sha-256',
-    username: 'ZnVuLXNoZWVwZG9nLTExMzM0JMOp3aflfWjISfAFuVHtd8NQeM_JZwj_PfEkHNU',
-    password: 'e93058a513ed4959bb249aab905b60f3',
+    username: KAFKA_USERNAME,
+    password: KAFKA_PASSWORD,
   },
   ssl: true,
 });
