d51e8a2dec89770212cc74fbcafdf6055d130307
bene <matheusbenediht@gmail.com>
2023-06-21 23:31:54 -0300

    Config Login

--- apps/login/src/application/middlewares/errorsCatch.ts
+++ apps/login/src/application/middlewares/errorsCatch.ts
@@ -1,14 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import 'express-async-errors';
 import { NextFunction, Request, Response } from 'express';
-export function errorsCatch(
-  error: Error,
+
+export default async (
+  error: { message: any },
   req: Request,
   res: Response,
   next: NextFunction,
-) {
+) => {
   return res.json({
     status: 'Error',
     message: error.message,
   });
-}
+};
