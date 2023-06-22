8c2540a814ab51065716db49f637855b00dc594a
bene <matheusbenediht@gmail.com>
2023-06-21 23:41:25 -0300

    Listando pedidos pelo ID

--- apps/order/src/infra/http/routes/order.ts
+++ apps/order/src/infra/http/routes/order.ts
@@ -2,11 +2,16 @@ import { Router } from 'express';
 import { CreateOrderController } from '../../../application/controllers/create-order.controller';
 import { IndexOrdersController } from '../../../application/controllers/index-order.controller';
 import { UpdateOrderController } from '../../../application/controllers/update-order-controller';
+import { GetOrderByIdController } from '../../../application/controllers/get-order.controller';
 
 const router = Router();
 
 router.get('/orders', (req, res) => {
   new IndexOrdersController().handle(req, res);
+});
+
+router.get('/order/:id', (req, res) => {
+  new GetOrderByIdController().handle(req, res);
 });
 
 router.post('/order/create', (req, res) => {
