9fc69d09fe2972423c57ceab213004faa4e7e4b3
bene <matheusbenediht@gmail.com>
2023-06-29 13:18:25 -0300

    Configuração do Multer

---
+++ apps/product/src/config/multer.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Options, diskStorage } from 'multer';
+import { extname, resolve } from 'path';
+import { randomBytes } from 'crypto';
+
+export const config = {
+  dest: resolve(__dirname, '..', '..', 'uploads', 'images'),
+  storage: diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, resolve(__dirname, '..', '..', 'uploads', 'images'));
+    },
+    filename: (req, file, cb) => {
+      randomBytes(16, (error, hash) => {
+        if (error) {
+          cb(error, file.filename);
+        }
+        const filename = `${hash.toString('hex')}${extname(file.originalname)}`;
+        cb(null, filename);
+      });
+    },
+  }),
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+};
